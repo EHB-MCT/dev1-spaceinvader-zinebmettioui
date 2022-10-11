046a4b9f7709fc8a0de8c53493934483abc235e3
zineb mettioui <zineb.mettioui@student.ehb.be>
2022-10-11 18:44:28 +0200

    Assignment 1: Space Invader

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,3 +1,42 @@
 "use strict";
 
-alert("Draw your space invader here");
+drawLine();
+
+function drawLine() {
+
+    let canvas = document.querySelector("canvas");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    let context = canvas.getContext("2d");
+
+
+    context.beginPath();
+    context.fillStyle = 'black';
+    context.beginPath();
+    context.rect(50,50,300,300);
+    context.fill();
+
+    context.beginPath(); 
+    context.fillStyle = '#66ff00';
+    context.beginPath();
+    context.rect(65,65,50,50);
+    context.rect(100,100,50,50);
+    context.rect(135,135,50,50);
+
+    context.rect(285,65,50,50);
+    context.rect(250,100,50,50);
+    context.rect(215,135,50,50);
+
+    context.rect(65,285,50,50);
+    context.rect(100,250,50,50);
+    context.rect(135,215,50,50);
+
+    context.rect(285,285,50,50);
+    context.rect(250,250,50,50);
+    context.rect(215,215,50,50);
+
+    context.rect(175,175,50,50);
+    context.fill();
+
+
+}
